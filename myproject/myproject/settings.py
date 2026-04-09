0b1e56b91cdfa92b9aa12b9b00d61031c15b0331
sumonghosh825 <sumonghosh892@gmail.com>
2026-04-09 21:11:10 +0530

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -48,7 +48,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'bcasumonghosh825@gmail.com'
-EMAIL_HOST_PASSWORD = 'oozx izfw xhqm pxwo'
+EMAIL_HOST_PASSWORD = ''
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
